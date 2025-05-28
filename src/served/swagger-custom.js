3b0328608c96b7463b884f5096fa089361e796fd
baophan894 <monkeyold113@gmail.com>
2025-05-28 13:26:59 +0700

    fix deploy

--- src/served/swagger-custom.js
+++ src/served/swagger-custom.js
@@ -3,7 +3,7 @@ async function postData(url, data = {}) {
 		method: 'POST',
 		mode: 'cors',
 		cache: 'no-cache',
-		credentials: 'same-origin',
+
 		headers: { 'Content-Type': 'application/json' },
 		redirect: 'follow',
 		referrerPolicy: 'no-referrer',
